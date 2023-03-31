c16dcd4a72a0891a68c4106b528d38fef414e936
Ray Hinch <rahinch@icloud.com>
2023-03-31 09:47:22 -0400

    Added user/userID route, started userSchema

--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -17,9 +17,29 @@ todoRouter.route('/')
     res.end('PUT operation not supported on /todos');
 })
 .delete((req, res) => {
+    res.statusCode = 403;
+    res.end('DEL operation not supported on /todos');
+});
+
+todoRouter.route('/:todoId')
+.get((req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Will delete all todos');
+    res.end('Will return todo with id: ' + req.params.todoId);
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /todos/' + req.params.todoId);
+})
+.put((req, res) => {    
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Will update todo with id: ' + req.params.todoId);
+})
+.delete((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Will delete todo with id: ' + req.params.todoId);
 });
 
 module.exports = todoRouter;
