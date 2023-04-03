8e260c49eceea7c782fb8debddbe44d1fcae98f7
Ray Hinch <rahinch@icloud.com>
2023-04-03 11:06:05 -0400

    added category to app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 
 const usersRouter = require('./routes/usersRouter');
 const todoRouter = require('./routes/todoRouter');
+const categoryRouter = require('./routes/categoryRouter');
 
 const app = express();
 
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
 app.use('/todos', todoRouter);
+app.use('/categories', categoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
